48e868cb3317b8d076dff6a7d19ef25c42e3e288
mwangashi <mwangashiw@gmail.com>
2024-05-08 08:05:55 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-class MagicStringCounter:
-    def __init__(self):
-        self.count = 0
-    
-    def magic_string(self):
-        self.count += 1
-        return "BestSchool" * self.count
-
-# Example Usage:
-counter = MagicStringCounter()
-print(counter.magic_string())  # Output: "BestSchool"
-print(counter.magic_string())  # Output: "BestSchoolBestSchool"
-print(counter.magic_string())  # Output: "BestSchoolBestSchoolBestSchool"
+def magic_string(G=[]):
+    G += ["BestSchool"]
+    return (", ".join(G))
