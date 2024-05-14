29197cf460f0ae20a4487ca868d12e91bd6288b8
mwangashi <mwangashiw@gmail.com>
2024-05-14 09:09:49 -0700

    3-is_kind_of_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
