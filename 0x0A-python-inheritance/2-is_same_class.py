b56ed6d7c979289384505b3c784ea22e8336122b
mwangashi <mwangashiw@gmail.com>
2024-05-14 09:03:57 -0700

    2-is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function"""
+
+
+def is_same_class(obj, a_class):
+    """an insatnce"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
